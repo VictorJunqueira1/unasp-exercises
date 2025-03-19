a1050e11f47225ae53dbfeff94583cef4f97ccd9
Victor Junqueira <victorjunqueira.prog@gmail.com>
2025-03-18 22:26:21 -0300

    [feat] Solucionando exercício 14 lista 2.

--- 1semestre/Python/2_condicionais/ex14.py
+++ 1semestre/Python/2_condicionais/ex14.py
@@ -2,6 +2,13 @@
 # recebe 05 bits individualmente de um número binário
 # e mostre o número decimal.
 
-# Ou seja, usuário pode inserir apenas 0 ou 1, 5 vezes
-# E retornar o número em decimal
+bit1 = int(input("Insira o primeiro bit: "))
+bit2 = int(input("Insira o segundo bit: "))
+bit3 = int(input("Insira o terceiro bit: "))
+bit4 = int(input("Insira o quarto bit: "))
+bit5 = int(input("Insira o quinto bit: "))
 
+# Se for 0, não contabiliza o bit
+
+decimal = (bit1 * 2**4) + (bit2 * 2**3) + (bit3 * 2**2) + (bit4 * 2**1) + (bit5 * 2**0)
+print(f"O número binário {bit1}{bit2}{bit3}{bit4}{bit5} em decimal é: {decimal}")
