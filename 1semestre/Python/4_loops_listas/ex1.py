2b9bf0a93dfa4388869b6541c02866fb9cb4e273
VictorJunqueira1 <victorjunqueira.prog@gmail.com>
2025-03-27 18:09:21 -0300

    [fix] Otimizando o processo para que percorra a lista apenas uma vez.

--- 1semestre/Python/4_loops_listas/ex1.py
+++ 1semestre/Python/4_loops_listas/ex1.py
@@ -7,19 +7,20 @@
 # matriz2 = [0]                                     => 0
 # matriz3 = [1, 1, 2]                               => 2
 # matriz4 = [0, 1, 0, 1, 0]                         => 0
-# matriz5 = [1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1] => 4
 
-# Utilizando O(n²)
+# Redução para O(n): Cada elemento da matriz é processado apenas duas vezes.
 
-matriz = [1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1]
+matriz5 = [ 1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1 ] # => 4
 
-contagem = {}
+elements = {}
 
-for num in matriz:
-    contagem = 0
-    for i in matriz:
-        if i == num:
-            contagem += 1
-    if contagem % 2 != 0:
-        print(f"O número que aparece um número ímpar de vezes é: {num}")
+for num in matriz5:
+    if num in elements:
+        elements[num] += 1
+    else:
+        elements[num] = 1
+
+for num, count in elements.items():
+    if count % 2 != 0:
+        print(f"O número que aparece um número ímpar de vezes na matriz é: {num}")
         break
--- 1semestre/Python/4_loops_listas/ex2.py
+++ 1semestre/Python/4_loops_listas/ex2.py
@@ -2,7 +2,14 @@
 # mostre a soma dos dois números positivos mais baixos, 
 # dada uma lista de no mínimo 4 números inteiros positivos.
 
-list = [25, 42, 12, 18, 22]
-smaller1, smaller2 = sorted(list)[:2]
+list = [ 25, 42, 12, 18, 22 ] # => 30
+smaller1, smaller2 = float('inf'), float('inf')
+
+for num in list:
+    if num < smaller1:
+        smaller2 = smaller1
+        smaller1 = num
+    elif num < smaller2:
+        smaller2 = num
 
 print(f"A soma dos dois números mais baixos é: {smaller1 + smaller2}")
