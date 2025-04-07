e1af3d66073988f999b4baba4b16e3a30ee7f240
Victor Junqueira <victorjunqueira.prog@gmail.com>
2025-04-07 17:39:46 -0300

    Update ex11.py

--- 1semestre/Python/5_functions/ex11.py
+++ 1semestre/Python/5_functions/ex11.py
@@ -1,4 +1,4 @@
-# 1. Crie uma função chamada quadrado que recebe dois
+# 11. Crie uma função chamada quadrado que recebe dois
 # parâmetros, um para a base e outro para a altura mostre
 # um quadrado feito por asteriscos.
 # Por exemplo, para os valores 5 e 3 a saída de ser:
